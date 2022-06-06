d9ee637e5cf260d87f9f2b61ddfcea8c2423dc4e
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-06 22:32:58 +0800

    Update fun-meme.js

--- plugins/fun-meme.js
+++ plugins/fun-meme.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-    let url = global.API('lolhuman', '/api/random/meme', {}, 'APIKEY')
+    let url = global.API('lolhuman', '/api/random/meme', {}, 'Papah-Chan')
     await conn.sendButtonImg(m.chat, url, '*MEME*', wm, 'NEXT', '.meme',m, 0, { thumbnail: await (await fetch(url)).buffer() })
 }
 handler.help = ['meme']
