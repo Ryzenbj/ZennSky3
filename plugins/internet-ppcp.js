42dbaf3f2167036ef475d7ff93b55b31d4233f3d
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-06 22:10:18 +0800

    Update internet-ppcp.js

--- plugins/internet-ppcp.js
+++ plugins/internet-ppcp.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, command }) => {
   let json = await res.json()
   if (!json.status) throw json
 conn.sendButtonImg(m.chat, json.result.female,  'Cewe', wm, 'Next', '.ppcp', m)
-conn.sendFile(m.chat, json.result.male,'pria.jpg',  'Cowo', m)
+conn.sendFile(m.chat, json.result.male,'pria',  'Cowo', m)
 
 }
 handler.help = ['ppcp']
