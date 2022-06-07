a2f32850ddbd9798f1e90d5c3f9ca0d348c233ce
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-07 19:40:34 +0800

    Update maker-wolf.js

--- plugins/maker-wolf.js
+++ plugins/maker-wolf.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
    if (!args[0]) return m.reply('masukkan text1 dan text2\n\nContoh: .wolflogo ren|funix')
    m.reply('*Wait Ngab*\nProses...')
   let res = `https://api.lolhuman.xyz/api/textprome2/wolflogo?apikey=Papah-Chan&text1=${response[0]}&text2=${response[1]}`
-  conn.sendFile(m.chat, res, 'wolflogo', `Sudah Jadi`, m, false)
+  conn.sendFile(m.chat, res, 'wolflogo.jpg', `Sudah Jadi`, m, false)
 }
 handler.help = ['wolflogo'].map(v => v + ' <text|text>')
 handler.tags = ['maker']
