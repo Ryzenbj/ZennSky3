df8b7f0b36f912954998f412a28ebe09200dd832
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-07 19:49:23 +0800

    Add files via upload

---
+++ plugins/maker-coolgravity.js
@@ -0,0 +1,13 @@
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args }) => {
+   response = args.join(' ').split('|')
+   if (!args[0]) return m.reply('masukkan text1 dan text2\n\nContoh: .coolgravity ren|funix')
+   m.reply('*Wait Ngab*\nProses...')
+  let res = `https://api.lolhuman.xyz/api/textprome2/coolgravity?apikey=Papah-Chan&text1=${response[0]}&text2=${response[1]}`
+  conn.sendFile(m.chat, res, 'coolgravity.jpg', `Sudah Jadi`, m, false)
+}
+handler.help = ['coolgravity'].map(v => v + ' <text|text>')
+handler.tags = ['maker']
+handler.command = /^(coolgravity)$/i
+
+module.exports = handler
