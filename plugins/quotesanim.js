528978c3fd74a1b12172967c5e28aad4a6e539b8
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-07 19:54:40 +0800

    Update quotesanim.js

--- plugins/quotesanim.js
+++ plugins/quotesanim.js
@@ -1,15 +1,15 @@
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
 
-	axios.get(`https://videfikri.com/api/anime/randomquoteanime`).then ((res) => {
+	axios.get(`https://api.lolhuman.xyz/api/random/quotesnime?apikey=Papah-Chan`).then ((res) => {
 	 	let hasil = `${res.data.result.quotes}`
 
     conn.reply(m.chat, hasil, m)
 	})
 }
-handler.help = ['quoteanime']
+handler.help = ['quotesanime']
 handler.tags = ['anime']
-handler.command = /^(quoteanime)$/i
+handler.command = /^(quotesanime)$/i
 handler.owner = false
 handler.mods = false
 handler.premium = false
