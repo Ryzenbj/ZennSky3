93681cdeeb65884d0df9c5182dbb7fd824cdaf21
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-17 08:37:56 +0800

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,12 +1,19 @@
-const hxz = require("hxz-api")
-let handler = async(m,{text, conn}) => {
+import { tiktokdl } from '@bochilteam/scraper'
 
-let p = await  hxz.ttdownloader(text)
-const { nowm, wm, audio } = p
-conn.sendFile(m.chat, nowm, null, 'Nih', m)
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `*TIKTOK DOWNLOADER*
+*Nickname:* ${nickname}
+*Description:* ${description}
+
+_© Elaina-ESM_
+`.trim(), m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(tiktok)$/i
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 
-module.exports = handler
+export default handler
